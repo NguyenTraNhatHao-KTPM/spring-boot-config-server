bde1c5bfa6f214afca0748f65b812a67adcad217
HaoSmall <nhathao00852@gmail.com>
2021-10-04 16:44:46 +0700

    first commit

---
+++ src/main/java/com/cloundconfigserver/CloundConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.cloundconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CloundConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloundConfigServerApplication.class, args);
+    }
+
+}
